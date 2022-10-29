df056dd2d1910bf89365f7010e1ed0ea211f211e
Allen Arani <allenarani@gmail.com>
2022-10-29 18:34:23 -0500

    updated frontend

---
+++ frontend/CodeRedPros.js
@@ -0,0 +1,6 @@
+
+formSubmission = document.getElementById("submitForm");
+
+formSubmission.addEventListener("click", function () {
+    location.href = "http://localhost:63342/CodeRedPros/frontend/presentationScreen.html"
+})
