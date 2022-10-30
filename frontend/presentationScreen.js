1959a7baf84f02a4807876f4d88f0b941c8d3476
Allen Arani <allenarani@gmail.com>
2022-10-30 03:40:00 -0500

    updated frontend added image assets

--- frontend/presentationScreen.js
+++ frontend/presentationScreen.js
@@ -4,16 +4,17 @@ let statCards = document.querySelectorAll(".statCard")
 let anchorOne = document.getElementById("anchorOne")
 let anchorTwo = document.getElementById("anchorTwo")
 let anchorThree = document.getElementById("anchorThree")
-let imgOne = document.getElementById("imgOne")
-let imgTwo = document.getElementById("imgTwo")
-let imgThree = document.getElementById("imgThree")
+let imgOne = document.getElementById("imageOne")
+let imgTwo = document.getElementById("imageTwo")
+let imgThree = document.getElementById("imageThree")
 
-let useCase = 4
+let useCase = 3
 
 statCards.forEach((statCard) => {
     statCard.addEventListener("click", () => {
         statCard.classList.toggle("isOpen");
         console.log("hovered");
+        setImages();
     })
 })
 
@@ -25,52 +26,47 @@ function setImages(){
             anchorOne.href = "https://store.steampowered.com/"
             anchorTwo.href = "https://us.shop.battle.net/en-us?from=root"
             anchorThree.href = "https://discord.com/"
-            imgOne.src = "assets/steam.png"
-            imgTwo.src = "assets/battlenet.png"
-            imgThree.src = "assets/discord.png"
+            imgOne.setAttribute("src", "assets/steam.png")
+            imgTwo.setAttribute("src", "assets/Battlenet.png")
+            imgThree.setAttribute("src", "assets/discord.png")
             break;
         case 2:
             anchorOne.href = "https://www.notion.com/"
             anchorTwo.href = "https://www.malwarebytes.com/"
             anchorThree.href = "https://www.spotify.com/"
-            imgOne.src = "assets/notion.png"
-            imgTwo.src = "assets/malwarebytes.png"
-            imgThree.src = "assets/spotify.png"
+            imgOne.setAttribute("src", "assets/Notion.png")
+            imgTwo.setAttribute("src", "assets/malwarebytes.png")
+            imgThree.setAttribute("src", "assets/spotify.png")
             break;
         case 3:
             anchorOne.href = "https://www.notion.com/"
             anchorTwo.href = "https://www.slack.com/"
             anchorThree.href = "https://www.office365.com/"
-            imgOne.src = "assets/notion.png"
-            imgTwo.src = "assets/slack.png"
-            imgThree.src = "assets/office365.png"
+            imgOne.setAttribute("src", "assets/Notion.png")
+            imgTwo.setAttribute("src", "assets/slack.png")
+            imgThree.setAttribute("src", "assets/office365.png")
             break;
         case 4:
             anchorOne.href = "https://www.canva.com/"
             anchorTwo.href = "https://www.figma.com/"
             anchorThree.href = "https://www.adobe.com/"
-            imgOne.src = "assets/canva.png"
-            imgTwo.src = "assets/figma.png"
-            imgThree.src = "assets/adobe.png"
+            imgOne.setAttribute("src", "assets/canva.png")
+            imgTwo.setAttribute("src", "assets/figma.png")
+            imgThree.setAttribute("src", "assets/adobe.png")
+
             break;
         case 5:
             anchorOne.href = "https://www.notion.com/"
             anchorTwo.href = "https://www.groupme.com/"
             anchorThree.href = "https://www.office365.com/"
-            imgOne.src = "assets/notion.png"
-            imgTwo.src = "assets/groupme.png"
-            imgThree.src = "assets/office365.png"
+            imgOne.setAttribute("src", "assets/Notion.png")
+            imgTwo.setAttribute("src", "assets/groupme.png")
+            imgThree.setAttribute("src", "assets/office365.png")
+
             break;
         default:
             break;
+        }
 
-    }
-
-
-
-
-    anchorOne.style.backgroundImage = "url(" + imgOne.src + ")";
-    anchorTwo.style.backgroundImage = "url(" + imgTwo.src + ")";
-    anchorThree.style.backgroundImage = "url(" + imgThree.src + ")";
 }
 
