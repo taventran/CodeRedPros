a12f26dc9b8b25c7ba4a92b30c738aeac5b20cb4
Allen Arani <allenarani@gmail.com>
2022-10-30 06:31:10 -0500

    Working on networking

--- frontend/CodeRedPros.js
+++ frontend/CodeRedPros.js
@@ -10,27 +10,39 @@ formSubmission.addEventListener("click", function () {
 
 
 function sendDataInput() {
+    let size;
+    let use;
+    let price;
+    let aesthetic;
     for(let i = 0; i < allLIs.length; i++){
         if(allLIs[i].classList.contains("active")){
            let id = allLIs[i].getAttribute("id");
-           let size;
-           let use;
-           let price;
+
            if(allLIs[i].classList.contains("pcSize")){
-               size = id;
+               size = id.substring(id.length-1);
            }
+           if(allLIs[i].classList.contains("budget")){
+                price = id.substring(id.length-1);
+            }
+            if(allLIs[i].classList.contains("usage")){
+                use = id.substring(id.length-1);
+            }
+            if (allLIs[i].classList.contains("aesthetic")) {
+                aesthetic = id.substring(id.length-1);
+            }
         }
     }
-
-
-
     let xhttp = new XMLHttpRequest();
-    xhttp.open("POST", `http://127.0.0.1:8000/api/${query}/?format=json`, false);
+    xhttp.open("POST", `http://127.0.0.1:8000/api/userdata/`, false);
+    xhttp.setRequestHeader("use", use)
+    xhttp.setRequestHeader("size", size)
+    xhttp.setRequestHeader("price", price)
+    xhttp.setRequestHeader("aesthetic", aesthetic)
     xhttp.send();
     if (xhttp.status !== 404) {
         let data = JSON.parse(xhttp.responseText);
         console.log(data)
-        return data;
+        return true;
     }
     return false;
 
