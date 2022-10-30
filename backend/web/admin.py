0948122e2fccf0364d29420fd9a167fb243b203b
Taven Tran <taventran@gmail.com>
2022-10-30 05:13:05 -0500

    Added models to admin

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from web.models import CPU, AllData, CPUCooler, GPU
+from web.models import CPU, AllData, CPUCooler, GPU, Memory, Storage, PowerSupply, Case
 
 # Register your models here.
 admin.site.register(CPU)
 admin.site.register(GPU)
 admin.site.register(CPUCooler)
+admin.site.register(Memory)
+admin.site.register(Storage)
+admin.site.register(PowerSupply)
+admin.site.register(Case)
 admin.site.register(AllData)
