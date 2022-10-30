684b17696279e54df8f0784f2a805eef82e1a9e5
Allen Arani <allenarani@gmail.com>
2022-10-30 06:16:43 -0500

    Working on networking

--- frontend/CodeRedPros.js
+++ frontend/CodeRedPros.js
@@ -1,20 +1,36 @@
 
 formSubmission = document.getElementById("submitForm");
+let allLIs = document.querySelectorAll("li");
+
 
 formSubmission.addEventListener("click", function () {
     location.href = "http://localhost:63342/CodeRedPros/frontend/presentationScreen.html"
-    getPartList("CPU");
+
 })
 
 
-function getPartList(query) {
+function sendDataInput() {
+    for(let i = 0; i < allLIs.length; i++){
+        if(allLIs[i].classList.contains("active")){
+           let id = allLIs[i].getAttribute("id");
+           let size;
+           let use;
+           let price;
+           if(allLIs[i].classList.contains("pcSize")){
+               size = id;
+           }
+        }
+    }
+
+
+
     let xhttp = new XMLHttpRequest();
-    xhttp.open("GET", `http://127.0.0.1:8000/api/${query}/?format=json`, false);
+    xhttp.open("POST", `http://127.0.0.1:8000/api/${query}/?format=json`, false);
     xhttp.send();
     if (xhttp.status !== 404) {
         let data = JSON.parse(xhttp.responseText);
         console.log(data)
-        return true;
+        return data;
     }
     return false;
 
