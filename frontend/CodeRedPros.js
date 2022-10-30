9de66a864362512a5c8b75af2c2d9ca464acf6e4
Allen Arani <allenarani@gmail.com>
2022-10-30 05:13:08 -0500

    updated frontend added image assets

--- frontend/CodeRedPros.js
+++ frontend/CodeRedPros.js
@@ -4,3 +4,17 @@ formSubmission = document.getElementById("submitForm");
 formSubmission.addEventListener("click", function () {
     location.href = "http://localhost:63342/CodeRedPros/frontend/presentationScreen.html"
 })
+
+
+function getStudySet(query) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET", `http://127.0.0.1:8000/api/${query}/?format=json`, false);
+    xhttp.send();
+    if (xhttp.status !== 404) {
+        let data = JSON.parse(xhttp.responseText);
+        console.log(data)
+        return true;
+    }
+    return false;
+
+}
