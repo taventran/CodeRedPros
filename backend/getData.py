7f6b0f5052600d2be837915a957a3aa787a6a8f3
Allen Arani <allenarani@gmail.com>
2022-10-30 07:48:59 -0500

    added parses

---
+++ backend/getData.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+import requests
+
+def getWeights(use, budget):
+    weights = ()
+
+    cpu = 0
+    cpu_cooler = 0
+    gpu = 0
+    ram = 0
+    storage = 0
+    mobo = 0
+    case = 0
+    psu = 0
+
+
+    if use == "1":
+        cpu = 0.2
+        cpu_cooler = 0.1
+        gpu = 0.2
+        ram = 0.1
+        storage = 0.1
+        mobo = 0.1
+        case = 0.1
+        psu = 0.1
+    elif use == "2":
+        cpu = 0.2
+        cpu_cooler = 0.05
+        gpu = 0.2
+        ram = 0.1
+        storage = 0.2
+        mobo = 0.1
+        case = 0.1
+        psu = 0.05
+    elif use == "3":
+        cpu = 0.2
+        cpu_cooler = 0.05
+        gpu = 0.2
+        ram = 0.1
+        storage = 0.2
+        mobo = 0.1
+        case = 0.1
+        psu = 0.05
+    elif use == "4":
+        cpu = 0.25
+        cpu_cooler = 0.05
+        gpu = 0.25
+        ram = 0.2
+        storage = 0.1
+        mobo = 0.05
+        case = 0.05
+        psu = 0.05
+    elif use == "5":
+        cpu = 0.2
+        cpu_cooler = 0.1
+        gpu = 0.25
+        ram = 0.2
+        storage = 0.1
+        mobo = 0.05
+        case = 0.05
+        psu = 0.05
+
+    weights = (cpu, cpu_cooler, gpu, ram, storage, mobo, case, psu) * budget
+    return weights
+
+
+def getCPU():
+    api_url = "https://127.0.0.1:8000/api/CPU/"
+    response = requests.get(api_url)
+    df = pd.DataFrame(pd.read_json(response.json()))
+    print(df.head())
+
+
+getCPU()
